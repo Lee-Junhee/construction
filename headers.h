e9513767f62e5b3b323cd88998185a276a0212b8
Lee-Junhee <junhee.lee002@gmail.com>
2019-10-15 22:43:27 -0400

    added required files

---
+++ headers.h
@@ -0,0 +1,9 @@
+struct {
+	int num;
+	char *name;
+	int banners;
+} team;
+
+team example();
+int printTeam(team source);
+int editTeam(team source);
---
+++ structs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"headers.h"
+
+team example() {
+	
+}
+
+int printTeam(team source) {
+
+}
+
+int editTeam(team source){
+
+}
